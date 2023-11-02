2e1ce0d71ee0c4513b44e0f159549c818f87a483
Dmitry <dnitri.kuznetsov@tinkoff.ru>
2023-11-02 15:21:21 +0500

    added base of forecast

---
+++ forecast.py
@@ -0,0 +1,35 @@
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+import okama as ok
+import pandas as pd
+import datetime
+
+
+def super_forecast(
+        ticket: str = 'MSFT.US',
+        n_days: int = 30,
+        last_period: int = 120) -> pd.DataFrame:
+
+    if n_days > 31:
+        print('Не рекомендуем строить прогноз более, чем на 1 месяц')
+
+    hist = ok.Asset(ticket).close_daily
+
+    data = pd.DataFrame({
+            'ds': pd.to_datetime(hist.index.astype(str)),
+            'y': hist.values.astype(float)
+            })
+
+    date_range = pd.date_range(
+        datetime.date.today() - pd.Timedelta(days=1),
+        datetime.date.today() + pd.Timedelta(days=n_days))
+
+    data = pd.concat([data, pd.DataFrame({'ds': date_range, 'y': np.nan})])
+    train, test = data[~data.y.isna()], data[data.y.isna()].drop(columns=['y'])
+    train = train[train.ds >= train.ds.max() - datetime.timedelta(days=last_period)]
+
+    model = ExponentialSmoothing(train['y']).fit()
+    preds = model.predict(start=test.index[0], end=test.index[-1])
+
+    test['y'] = preds.reset_index(drop=True)
+
+    return pd.concat([data.dropna(), test])
